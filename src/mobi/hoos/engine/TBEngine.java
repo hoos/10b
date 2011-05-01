7120b7b33ce7686fb731afa4e01ad024eb6ccc2d
Hussein Badakhchani <husseinb@localhost.localdomain>
2011-05-01 01:35:24 +0100

    Added command line parsing library

--- src/mobi/hoos/engine/TBEngine.java
+++ src/mobi/hoos/engine/TBEngine.java
@@ -2,6 +2,18 @@ package mobi.hoos.engine;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Parser;
+import org.apache.commons.cli.ParseException;
+
 
 /**
  * The TBEngine class orchestrates all data analysis activities.
@@ -9,11 +21,40 @@ import java.util.logging.Level;
  */
 public class TBEngine {
 
-    private final static Logger logger = Logger.getLogger(TBEngine.class.getName());
+    private static FileHandler fileHandler = null; 
+    private static final Logger logger = Logger.getLogger(TBEngine.class.getName());
+
+    // Initialise logging.
+    static {
+        try {
+            TBEngine.fileHandler = new FileHandler("/home/husseinb/Projects/10b/dist/10b/log/10b.log");
+            logger.addHandler(fileHandler); 
+            logger.setUseParentHandlers(false);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to create log file handler: ./log/10.log");
+        }
+    }
 
     public static void main(String[] args) {
-        TBEngine.logger.setLevel(Level.INFO);
         logger.log(Level.INFO, "Starting 10b!");
+
+        // Create Options object
+        Options options = new Options();
+
+        // Setup options
+        Option help = new Option("help", "print this message");
+        Option version = new Option("version", "print the version information and exit");
+        Option quiet = new Option("quiet", "be extra quiet");
+        Option verbose = new Option("quiet", "be extra verbose");
+
+        // Create the command line parser
+        CommandLineParser parser = new GnuParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args );
+        }  catch( ParseException exp ) {
+            logger.log(Level.SEVERE, "Command line parsing failed: " + exp.getMessage() );
+        }
     }
 
 }
