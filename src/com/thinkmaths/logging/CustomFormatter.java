8de9f437d7c8a86217e8f3d90401ebf29367c467
Hussein Badakhchani <husseinb@localhost.localdomain>
2011-05-09 18:25:24 +0100

    General updates

--- src/com/thinkmaths/logging/CustomFormatter.java
+++ src/com/thinkmaths/logging/CustomFormatter.java
@@ -1,8 +1,8 @@
 /*
  * Created Jul 8, 2008
- * 
+ *
  * Copyright ThinkTank Maths Limited 2008
- * 
+ *
  * This file is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
@@ -16,9 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this file. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package com.thinktankmaths.logging;
- 
+
 import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
@@ -26,7 +26,7 @@ import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
- 
+
 /**
  * A {@link Formatter} that may be customised in a {@code logging.properties}
  * file. The syntax of the property
@@ -34,53 +34,61 @@ import java.util.logging.LogRecord;
  * specifies the output. A newline will be appended to the string and the
  * following special characters will be expanded (case sensitive):-
  * <ul>
- * <li>{@code %m} - message</li>
  * <li>{@code %L} - log level</li>
+ * <li>{@code %m} - message</li>
  * <li>{@code %n} - name of the logger</li>
- * <li>{@code %t} - a timestamp (in ISO-8601 "yyyy-MM-dd HH:mm:ss Z" format)</li>
+ * <li>{@code %t} - a timestamp in ISO-8601 "yyyy-MM-dd HH:mm:ss Z" format</li>
  * <li>{@code %M} - source method name (if available, otherwise "?")</li>
  * <li>{@code %c} - source class name (if available, otherwise "?")</li>
  * <li>{@code %C} - source simple class name (if available, otherwise "?")</li>
  * <li>{@code %T} - thread ID</li>
  * </ul>
- * The default format is {@value #DEFAULT_FORMAT}. Curly brace characters are not
- * allowed.
- * 
+ * The default format is {@value #DEFAULT_FORMAT}. Curly brace characters are 
+ * not allowed.
+ *
  * @author Samuel Halliday
+ * @author Hussein Badakhchani
  */
 public class CustomFormatter extends Formatter {
     private static final String DEFAULT_FORMAT = "%L: %m [%c.%M %t]";
- 
+    private static final int LOGLEVEL = 0;
+    private static final int MESSAGE = 1;
+    private static final int LOGGER = 2;
+    private static final int TIMESTAMP = 3;
+    private static final int METHOD = 4;
+
     private final MessageFormat messageFormat;
- 
+
     private final DateFormat dateFormat =
         new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
- 
+
     /** */
     public CustomFormatter() {
         super();
- 
+
         // load the format from logging.properties
         String propName = getClass().getName() + ".format";
         String format = LogManager.getLogManager().getProperty(propName);
-        if (format == null || format.trim().length() == 0)
+        if (format == null || format.trim().length() == 0) {
             format = DEFAULT_FORMAT;
-        if (format.contains("{") || format.contains("}"))
+        }
+        if (format.contains("{") || format.contains("}")) {
             throw new IllegalArgumentException("curly braces not allowed");
- 
+        }
         // convert it into the MessageFormat form
-        format = format.replace("%L", "{0}").replace("%m", "{1}").replace("%M",
-            "{2}").replace("%t", "{3}").replace("%c", "{4}").replace("%T", "{5}").
-            replace("%n", "{6}").replace("%C", "{7}") + "\n";
- 
+        format = format.replace("%L", "{0}").replace("%m", "{1}").
+            replace("%M", "{2}").replace("%t", "{3}").replace("%c", "{4}").
+            replace("%T", "{5}").replace("%n", "{6}").
+            replace("%C", "{7}") + "\n";
+
         messageFormat = new MessageFormat(format);
     }
- 
+
     @Override
-    public String format(LogRecord record) {
+    public final String format(LogRecord record) {
         String[] arguments = new String[8];
         // %L
-        arguments[0] = record.getLevel().toString();
+        arguments[CustomFormatter.LOGLEVEL] = record.getLevel().toString();
         // %m
         arguments[1] = record.getMessage();
         // sometimes the message is empty, but there is a throwable
@@ -99,13 +107,13 @@ public class CustomFormatter extends Formatter {
         // %t
         Date date = new Date(record.getMillis());
         synchronized (dateFormat) {
-            arguments[3] = dateFormat.format(date);
+            arguments[CustomFormatter.TIMESTAMP] = dateFormat.format(date);
         }
         // %c
         if (record.getSourceClassName() != null) {
-            arguments[4] = record.getSourceClassName();
+            arguments[CustomFormatter.METHOD] = record.getSourceClassName();
         } else {
-            arguments[4] = "?";
+            arguments[CustomFormatter.METHOD] = "?";
         }
         // %T
         arguments[5] = Integer.valueOf(record.getThreadID()).toString();
@@ -118,7 +126,7 @@ public class CustomFormatter extends Formatter {
         } else {
             arguments[7] = arguments[4];
         }
- 
+
         synchronized (messageFormat) {
             return messageFormat.format(arguments);
         }
