7391f1c921e3ee57cddffeac2bfc496897afe83c
Hussein Badakhchani <husseinb@localhost.localdomain>
2011-05-29 22:31:41 +0100

    patched up build.sh to use the bundled ANT version

--- build.sh
+++ build.sh
@@ -8,10 +8,8 @@
 #              10b.  All the arguments that are supplied to this script
 #              are passed on to Ant.
 
-BIN_DIR=`/bin/env dirname $0`
-APPLICATION_HOME=$BIN_DIR/../
+APPLICATION_HOME=`/bin/env dirname $0`
+#APPLICATION_HOME=$BIN_DIR
 ANT_HOME=$APPLICATION_HOME/thirdparty/apache-ant-1.8.2
 
-PATH=$ANT_HOME/bin:$PATH
-
-ant -buildfile $BIN_DIR/build.xml $*
+$ANT_HOME/bin/ant -buildfile $APPLICATION_HOME/build.xml $*
