d47f2d8bc62974871d27a8668989a0e8c84320a8
Hussein Badakhchani <husseinb@localhost.localdomain>
2011-05-30 06:05:41 +0100

    Fixed checkstyle errors

--- src/mobi/hoos/engine/TBEngineTest.java
+++ src/mobi/hoos/engine/TBEngineTest.java
@@ -3,7 +3,6 @@ package mobi.hoos.engine;
 import junit.framework.TestCase;
 import mobi.hoos.dataset.DataSet;
 import mobi.hoos.dataset.DataSetFactory;
-import mobi.hoos.engine.TBEngine;
 
 /**
  * The TBEngineTest class is the Junit test case suite for TBEngine.
@@ -13,11 +12,14 @@ public class TBEngineTest extends TestCase {
 
     private TBEngine tbEngine; 
 
+    /**
+     * Set up the test case by creating an instance of the Two Bit Engine class.
+     */
     public void setUp() {
-        this.tbEngine = new TBEngine(); 
+        this.tbEngine = new TBEngine();
     }
 
     public void testGetDataSet() throws Exception {
-        DataSet dataSet = this.tbEngine.getDataSet(DataSetFactory.INTEGER_TYPE); 
-    } 
+        DataSet dataSet = this.tbEngine.getDataSet(DataSetFactory.INTEGER_TYPE);
+    }
 }
