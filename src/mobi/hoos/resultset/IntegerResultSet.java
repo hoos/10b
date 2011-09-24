762e6d8b5703860260318969bf2e6a3b728ab5d5
Hussein Badakhchani <hoos@email.com>
2011-09-24 15:05:53 +0100

    Added result set

---
+++ src/mobi/hoos/resultset/IntegerResultSet.java
@@ -0,0 +1,9 @@
+package mobi.hoos.resultset;
+
+/**
+ * The IntegerRsultSet class manages integer data.
+ * @author Hussein Badakhchani
+ */
+public class IntegerResultSet implements ResultSet {
+
+}
